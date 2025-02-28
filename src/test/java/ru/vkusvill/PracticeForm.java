cd26f10d1d0bb7703d73057e9939035e0b8f0210
Oleg Borzunov <olezhoncsgo@gmail.com>
2025-02-28 12:18:31 +0500

    Наполнена кодовая база на исполнение сценария

--- src/test/java/ru/vkusvill/PracticeForm.java
+++ src/test/java/ru/vkusvill/PracticeForm.java
@@ -3,8 +3,11 @@ package ru.vkusvill;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.io.File;
 
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -22,15 +25,39 @@ public class PracticeForm {
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
-        $x("//input[@id='firstName']").setValue("Boba");
-        $x("//input[@id='lastName']").setValue("Fedotov");
-        $x("//input[@id='userEmail']").setValue("BobaFedotovLordofLogs@bibaboba.ru");
-        $x("//label[@for='gender-radio-3']").click();
-        $x("//input[@id='userNumber']").setValue("88005553535");
-        //Дата рождения (будет вечером)
-        $x("//input[@id='subjectsInput']").setValue("Phys").pressEnter();
-        $x("//label[@for='hobbies-checkbox-3']").click();
-        $x("//textarea[@id='currentAddress']").setValue("citizen of the world");
+        $x("//input[@id='firstName']").setValue("Boba");    //Ввод имени
+        $x("//input[@id='lastName']").setValue("Fedotov");  //Ввод фамилии
+        $x("//input[@id='userEmail']").setValue("BobaFedotov@mail.ru"); //Ввод email
+        $x("//label[@for='gender-radio-3']").click(); //Выбор гендера
+        $x("//input[@id='userNumber']").setValue("88005553535");    //Ввод номера телефона
+        $x("//input[@id='dateOfBirthInput']").click(); //Ввод даты рождения
+        $x("//select[contains(@class, 'react-datepicker__year-select')]").selectOptionByValue("2000");
+        $x("//select[contains (@class, 'react-datepicker__month-select')]").selectOptionByValue("9");
+        $x("//div[contains(@class, 'react-datepicker__day--017') and text()='17']").click();
+        $x("//input[@id='subjectsInput']").setValue("Phys").pressEnter(); //Выбор предмета
+        $x("//label[@for='hobbies-checkbox-3']").click(); //Выбор хобби
+        $x("//textarea[@id='currentAddress']").setValue("citizen of the world"); //Ввод адреса
+        $x("//input[@id='uploadPicture']").uploadFromClasspath("1.png");//Загрузка файла
+
+        // setTimeout(function() {debugger},4000); // Скрипт для холда devtools
+        $x("//div[@class=' css-1wa3eu0-placeholder']").scrollIntoView(true);//Скролл до выбора штата
+
+        $x("//div[contains(@class, ' css-1wa3eu0-placeholder')]").click(); //Нажатие на выбор штата
+        $x("//div[contains(@id, 'react-select-3-option-1')]").click(); //Выбор штата
+        $x("//div[contains(@id, 'city')]").click(); //Нажатие на выбор города
+        $x("//div[contains(@id, 'react-select-4-option-0')]").click(); //Нажатие на выбор города
+        $x("//button[contains(@id, 'submit')]").click(); //Нажатие на кнопку "Submit"
+
+        //Проверки
+        $x("//div[contains(@class, 'modal-dialog modal-lg')]").should(appear); //Проверка появления формы успешного заполнения
+        $x("//div[contains(@id, 'example-modal-sizes-title-lg')]").shouldHave(text("Thanks for submitting the form")); //Проверка появления формы успешного заполнения
+        $x("//div[contains(@class, 'modal-body')]").shouldHave(text("Boba Fedotov"), text("BobaFedotov@mail.ru"), text("8800555353")); //Проверка правильности заполнения формы
+
+
+
+
+
+
 
     }
 }

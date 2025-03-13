51a3b184daf59517ca9ef1bc19477c421b5fda06
Oleg Borzunov <olezhoncsgo@gmail.com>
2025-03-13 14:00:49 +0500

    Добавление BeforeAll

--- src/test/java/ru/vkusvillTests/TestBase.java
+++ src/test/java/ru/vkusvillTests/TestBase.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.BeforeAll;
 public class TestBase {
     @BeforeAll
     static void configuration() {
-
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";

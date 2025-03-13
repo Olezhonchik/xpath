5a7543bd1df1b63a577c3bf3c9971936a30e8107
Oleg Borzunov <olezhoncsgo@gmail.com>
2025-03-13 13:55:09 +0500

    Визуальная оптимизация

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -9,6 +9,5 @@ public class CalendarComponent {
         $x("//select[contains(@class, 'react-datepicker__year-select')]").selectOptionByValue("2000");
         $x("//select[contains (@class, 'react-datepicker__month-select')]").selectOptionByValue("9");
         $x("//div[contains(@class, 'react-datepicker__day--017') and text()='17']").click();
-
     }
 }
